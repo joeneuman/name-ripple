2ee4c682926abdc9ca2e0b8b4ecc930c3506941a
Joe Neuman <joeneuman@mystg.com>
2025-02-27 09:23:58 -0700

    Simplify Vite config with relative paths

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/name-ripple/',
+  base: './',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: 'dist'
   }
 })
